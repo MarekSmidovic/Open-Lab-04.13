5d3a87a27edde75bd47482682ef948ebfcc819c6
smidovic <msmidovic@gmail.com>
2021-11-24 10:29:05 +0100

    funnkcia pismena

--- Open-Lab-04.13/StringTools.cs
+++ Open-Lab-04.13/StringTools.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Open_Lab_04._13
 {
@@ -6,7 +7,21 @@ namespace Open_Lab_04._13
     {
         public string GetLongestCommonSequence(string str1, string str2)
         {
-            throw new NotImplementedException();
+            char[] pismena1 = str1.ToCharArray();
+            char[] pismena2 = str2.ToCharArray();
+            string vysledok = string.Empty;
+
+            for (int i = 0; i < pismena1.Length - 1; i++)
+            {
+                for (int j = 0; j < pismena2.Length - 1; j++)
+                {
+                    if (pismena1[i] == pismena2[j])
+                    {
+                        vysledok = vysledok + pismena1[i].ToString();
+                    }
+                }
+            }
+            return vysledok;
         }
     }
 }
